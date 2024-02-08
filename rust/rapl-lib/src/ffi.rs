84fdf5dd399338f4c4c865b20d8305828ff1578a
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-02-08 10:45:13 +0100

    Clippy fixes

--- rust/rapl-lib/src/ffi.rs
+++ rust/rapl-lib/src/ffi.rs
@@ -1,16 +1,22 @@
 use crate::rapl;
 use std::ffi::{c_char, CStr};
 
+// # Safety
+//
+// This function is unsafe because it dereferences the `id` pointer.
 #[no_mangle]
-pub extern "C" fn start_rapl(id: *const c_char) {
+pub unsafe extern "C" fn start_rapl(id: *const c_char) {
     let id_cstr = unsafe { CStr::from_ptr(id) };
     let id_string = String::from_utf8_lossy(id_cstr.to_bytes()).to_string();
     println!("start_rapl id: {}", id_string);
     rapl::start_rapl();
 }
 
+// # Safety
+//
+// This function is unsafe because it dereferences the `id` pointer.
 #[no_mangle]
-pub extern "C" fn stop_rapl(id: *const c_char) {
+pub unsafe extern "C" fn stop_rapl(id: *const c_char) {
     let id_cstr = unsafe { CStr::from_ptr(id) };
     let id_string = String::from_utf8_lossy(id_cstr.to_bytes()).to_string();
     println!("stop_rapl id: {}", id_string);
