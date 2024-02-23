f204249cd3fa467dfc746f13fe22c60702fd48df
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-02-23 13:03:39 +0100

    doc and cleanup stuff

--- rust/rapl-bin/src/main.rs
+++ rust/rapl-bin/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use rapl_lib::ffi::{start_rapl, stop_rapl};
-use std::ffi::CString;
+use std::{ffi::CString, thread, time::Duration};
 
 fn main() -> Result<()> {
     // Call start and stop rapl 500 times
@@ -8,10 +8,10 @@ fn main() -> Result<()> {
     // Get current time in milliseconds
     let start = std::time::Instant::now();
 
-    // spawn 10 threads to run the test function
+    // spawn 10 threads to run the benchmark function
     let handles: Vec<_> = (0..10)
         .map(|i| {
-            std::thread::spawn(move || {
+            thread::spawn(move || {
                 let test_function = CString::new(format!("test_function {}", i)).unwrap();
 
                 for _ in 0..100000 {
@@ -31,7 +31,7 @@ fn main() -> Result<()> {
     println!("Start and stop rapl benchmark: {}ms", end);
 
     // Sleep for 5 seconds to allow writing to CSV
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(5));
 
     Ok(())
 }
